e284a05e89627dd879dcd2933d8b00d1d7ad7cdf
lijk <lijk@rd.netease.com>
2017-06-13 21:44:03 +0800

    complete OKHttpStack

--- app/src/main/java/com/lijiankun24/volleypractice/volley/OkHttpStack.java
+++ app/src/main/java/com/lijiankun24/volleypractice/volley/OkHttpStack.java
@@ -4,10 +4,32 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.toolbox.HttpStack;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import static okhttp3.Protocol.HTTP_1_0;
+import static okhttp3.Protocol.HTTP_1_1;
+import static okhttp3.Protocol.HTTP_2;
+import static okhttp3.Protocol.SPDY_3;
 
 /**
  * OkHttpStack.java
@@ -17,8 +39,134 @@ import java.util.Map;
 
 public class OkHttpStack implements HttpStack {
 
+    private final OkHttpClient mOkHttpClient;
+
+    public OkHttpStack(OkHttpClient okHttpClient) {
+        if (okHttpClient == null) {
+            throw new IllegalArgumentException("OkHttpClient can't be null");
+        }
+        mOkHttpClient = okHttpClient;
+    }
+
     @Override
-    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders) throws IOException, AuthFailureError {
-        return null;
+    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders)
+            throws IOException, AuthFailureError {
+        int timeoutMs = request.getTimeoutMs();
+        OkHttpClient okHttpClient = mOkHttpClient
+                .newBuilder()
+                .connectTimeout(timeoutMs, TimeUnit.MILLISECONDS)
+                .readTimeout(timeoutMs, TimeUnit.MILLISECONDS)
+                .writeTimeout(timeoutMs, TimeUnit.MILLISECONDS)
+                .build();
+
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.putAll(request.getHeaders());
+        headers.putAll(additionalHeaders);
+
+        okhttp3.Request.Builder builder = new okhttp3.Request
+                .Builder()
+                .url(request.getUrl());
+        for (String key : headers.keySet()) {
+            builder.header(key, headers.get(key));
+        }
+        setConnectionParametersForRequest(builder, request);
+
+        okhttp3.Request okRequest = builder.build();
+        Call call = okHttpClient.newCall(okRequest);
+        Response okResponse = call.execute();
+        BasicStatusLine responseStatus = new BasicStatusLine(
+                parseProtocol(okResponse.protocol()),
+                okResponse.code(),
+                okResponse.message()
+        );
+
+        BasicHttpResponse response = new BasicHttpResponse(responseStatus);
+        response.setEntity(entityFromOkHttpResponse(okResponse));
+
+        Headers responseHeaders = okResponse.headers();
+        int size = responseHeaders.size();
+        String name;
+        String value = null;
+        for (int i = 0; i < size; i++) {
+            name = responseHeaders.name(i);
+            if (name != null) {
+                response.addHeader(new BasicHeader(name, value));
+            }
+        }
+        return response;
+    }
+
+    private void setConnectionParametersForRequest(okhttp3.Request.Builder builder, Request<?> request)
+            throws AuthFailureError {
+        switch (request.getMethod()) {
+            case Request.Method.DEPRECATED_GET_OR_POST:
+                byte[] postBody = request.getPostBody();
+                if (postBody != null) {
+                    builder.post(RequestBody.create(MediaType.parse(request.getPostBodyContentType()), postBody));
+                }
+                break;
+            case Request.Method.GET:
+                builder.get();
+                break;
+            case Request.Method.DELETE:
+                builder.delete();
+                break;
+            case Request.Method.POST:
+                builder.post(createRequestBody(request));
+                break;
+            case Request.Method.PUT:
+                builder.put(createRequestBody(request));
+                break;
+            case Request.Method.HEAD:
+                builder.head();
+                break;
+            case Request.Method.OPTIONS:
+                builder.method("OPTIONS", null);
+                break;
+            case Request.Method.TRACE:
+                builder.method("TRACE", null);
+                break;
+            case Request.Method.PATCH:
+                builder.patch(createRequestBody(request));
+                break;
+            default:
+                throw new IllegalStateException("Unknown method type.");
+        }
+    }
+
+    private static RequestBody createRequestBody(Request<?> r) throws AuthFailureError {
+        final byte[] body = r.getBody();
+        if (body == null) return null;
+
+        return RequestBody.create(MediaType.parse(r.getBodyContentType()), body);
+    }
+
+    private static ProtocolVersion parseProtocol(final Protocol p) {
+        switch (p) {
+            case HTTP_1_0:
+                return new ProtocolVersion("HTTP", 1, 0);
+            case HTTP_1_1:
+                return new ProtocolVersion("HTTP", 1, 1);
+            case SPDY_3:
+                return new ProtocolVersion("SPDY", 3, 1);
+            case HTTP_2:
+                return new ProtocolVersion("HTTP", 2, 0);
+        }
+
+        throw new IllegalAccessError("Unkwown protocol");
+    }
+
+    private static HttpEntity entityFromOkHttpResponse(Response r) throws IOException {
+        BasicHttpEntity entity = new BasicHttpEntity();
+        ResponseBody body = r.body();
+
+        entity.setContent(body.byteStream());
+        entity.setContentLength(body.contentLength());
+        entity.setContentEncoding(r.header("Content-Encoding"));
+
+        if (body.contentType() != null) {
+            entity.setContentType(body.contentType().type());
+        }
+        return entity;
     }
 }
